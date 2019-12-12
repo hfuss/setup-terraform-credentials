e72a9e270426df99ec3a2403b5c6e4d19fbb7bd8
Hayden Fuss <wifu1234@gmail.com>
2019-12-11 22:59:50 -0500

    working action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import { templateTerraformCredentials } from './terraform';
 
-async function run(): Promise<void> {
+async function run() {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await templateTerraformCredentials()
   } catch (error) {
     core.setFailed(error.message)
   }
 }
 
-run()
+run();
